c86a4452c3e97039fa62f8a61c011c27e55e00a7
Eng-Prud <Prudence.n826@gmai.com>
2024-02-01 12:05:45 -0800

    rectify

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,11 +4,4 @@
 #The receiver phone number or name (including country code if present)
 #The flags that were used
 
-# Read the log file provided as input
-log_file = ARGF.read
-
-# Extract sender, receiver, and flags from each log entry
-log_file.scan(/\[from:([\w\s\+]+)\] \[to:([\w\s\+]+)\] \[flags:(.*?)\]/) do |sender, receiver, flags|
-  puts "#{sender},#{receiver},#{flags}"
-end
-
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
