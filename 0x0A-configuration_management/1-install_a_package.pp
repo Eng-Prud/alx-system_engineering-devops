375304ab711568ee58f5918fa796e5db37577777
Eng-Prud <Prudence.n826@gmai.com>
2024-03-22 13:53:41 -0700

    remove empty line

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-# Puppet manifest to install Flask version 2.1.0 using pip3
-
 # Install a specific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-}
+  provider => 'pip3'
 }
