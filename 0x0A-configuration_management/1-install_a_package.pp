94a08ddc9061fc93c04655f66b87b734d06a68e4
Eng-Prud <Prudence.n826@gmai.com>
2024-03-22 13:51:15 -0700

    start codes with a comment

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-#!/usr/bin/pup
-#Install a specific version of flask (2.1.0)
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Install a specific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
 }
